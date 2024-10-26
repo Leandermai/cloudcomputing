a818445176c9c2ce81a46472e967b57075fb3c98
lm/223994 <leander.maigler@hs-weingarten.de>
2024-10-26 11:17:48 +0200

    modified:   comments.html 	modified:   index.html 	modified:   package-lock.json 	modified:   package.json 	modified:   server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const exp = require('constants');
 const express = require('express');
 const path = require('path');
 
@@ -7,6 +8,39 @@ const PORT = 2000;
 // Serve the index.html file when the root URL is accessed
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Serve the comments.html file 
+app.get('/comments', (req, res) => {
+    res.sendFile(path.join(__dirname, 'comments.html'));
+});
+
+// Serve the showdcomments.html file
+app.get('/showcomments', (req, res) => {
+    res.sendFile(path.join(__dirname, 'showcomments.html'));
+});
+
+
+
+//Array to store the comments
+let comments = [];
+
+//Middleware to parse JSON data
+app.use(express.json());
+
+//Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Post endpoint to receive comments
+app.post('/comments', (req, res) => {
+    const { name, comment } = req.body;
+
+    //Save the comment in the comments adday
+    const newComment = { name, comment };
+    comments.push(newComment);
+
+    //Send the saved comment back to client
+    res.join(newComment);
 });
 
 // Start the server
