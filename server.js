8e19ec6e6581b7d878a639d2754c52e1079fc2f0
js-224680 <juri.samarskij@hs-weingarten.de>
2024-10-26 14:17:52 +0200

    bis jetzt

--- server.js
+++ server.js
@@ -1,38 +1,47 @@
+<<<<<<< HEAD
 const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = 2000;
+=======
+  const express = require('express');
+  const path = require('path');
 
-// Serve the index.html file when the root URL is accessed
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
+  const app = express();
+  const PORT = 2000;
+>>>>>>> c38b369 (bis jetzt)
 
-// Serve the comments.html file 
-app.get('/comments', (req, res) => {
-    res.sendFile(path.join(__dirname, 'comments.html'));
-});
+  // Serve the index.html file when the root URL is accessed
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+  });
 
-// Serve the showcomments.html file
-app.get('/showcomments', (req, res) => {
-    res.sendFile(path.join(__dirname, 'showcomments.html'));
-});
+  // Serve the comments.html file 
+  app.get('/comments', (req, res) => {
+      res.sendFile(path.join(__dirname, 'comments.html'));
+  });
 
-// Redirect URLs with .html extension to the same path without .html
-app.get('/*.html', (req, res) => {
-  const pathWithoutExtension = req.path.slice(0, -5); // removes ".html"
-  res.redirect(301, pathWithoutExtension);
-});
+  // Serve the showcomments.html file
+  app.get('/showcomments', (req, res) => {
+      res.sendFile(path.join(__dirname, 'showcomments.html'));
+  });
 
-// Array to store the comments
-let comments = [];
+  // Redirect URLs with .html extension to the same path without .html
+  app.get('/*.html', (req, res) => {
+    const pathWithoutExtension = req.path.slice(0, -5); // removes ".html"
+    res.redirect(301, pathWithoutExtension);
+  });
 
-// Middleware to parse JSON data
-app.use(express.json());
+  // Array to store the comments
+  let comments = [];
 
-// Serve static files from the 'comments' directory
-app.use(express.static(path.join(__dirname, 'comments')));
+  // Middleware to parse JSON data
+  app.use(express.json());
 
+  // Serve static files from the 'comments' directory
+  app.use(express.static(path.join(__dirname, 'comments')));
+
+<<<<<<< HEAD
 // POST endpoint to receive comments
 app.post('/comments', (req, res) => {
     const { name, comment } = req.body;
@@ -50,10 +59,26 @@ app.get('/showcomments', (res, req) => {
 
 // GET endpoint for comments to show in showcomments.html
 app.get('/comments', (req, res) => {
-    res.json(comments);
-});
+=======
+  // POST endpoint to receive comments
+  app.post('/comments', (req, res) => {
+      const { name, comment } = req.body;
 
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+      // Save the comment in the comments array
+      const newComment = { name, comment };
+      comments.push(newComment);
+
+      // Send the saved comment back to client
+      res.json(newComment);
+  });
+
+    // GET endpoint for comments to show in showcomments.html
+  app.get('/comments', (req, res) => {
+>>>>>>> c38b369 (bis jetzt)
+    res.json(comments);
+  });
+
+  // Start the server
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
